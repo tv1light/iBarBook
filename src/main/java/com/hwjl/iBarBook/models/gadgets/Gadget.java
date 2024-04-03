21b804b16919969247507874ac568e0c4e4adf22
2ShKiPeR <igormokhnatov78@gmail.com>
2024-04-03 20:14:22 +0400

    added class description

--- src/main/java/com/hwjl/iBarBook/models/gadgets/Gadget.java
+++ src/main/java/com/hwjl/iBarBook/models/gadgets/Gadget.java
@@ -1,4 +1,21 @@
 package com.hwjl.iBarBook.models.gadgets;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name = "Gadget")
+@Table(name = "Gadgets")
 public class Gadget {
+    @Id
+    @Column(name = "gadget_id")
+    private Long gadget_id;
+
+    @Column(name = "gadget_name", nullable = false, unique = true)
+    private String gadget_name;
+
+    @Column(name = "description", nullable = false, unique = true)
+    private String description;
+
 }
--- src/main/java/com/hwjl/iBarBook/models/roles/Role.java
+++ src/main/java/com/hwjl/iBarBook/models/roles/Role.java
@@ -1,4 +1,17 @@
 package com.hwjl.iBarBook.models.roles;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name = "Role")
+@Table(name = "Roles")
 public class Role {
+    @Id
+    @Column(name = "role_id")
+    private Long role_id;
+
+    @Column(name = "role_name", nullable = false, unique = true)
+    private String role_name;
 }
