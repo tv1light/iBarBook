bc3604f4b40454f640592049009a5b8bb69126f8
Vlad <vlodik.guterian@gmail.com>
2024-04-04 16:00:39 +0400

    Add interfaces

--- src/main/java/com/hwjl/iBarBook/models/cocktails/Cocktail.java
+++ src/main/java/com/hwjl/iBarBook/models/cocktails/Cocktail.java
@@ -1,5 +1,23 @@
 package com.hwjl.iBarBook.models.cocktails;
 
-public class Cocktail {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity(name = "Cocktail")
+@Table(name = "Cocktails")
+public class Cocktail {
+    @Id
+    @Column(name = "cocktail_id")
+    private Long cocktail_id;
+
+    @Column(name = "cocktail_name", nullable = false, unique = true)
+    private String cocktail_name;
+
+    @Column(name = "description", nullable = false, unique = true)
+    private String description;
+
+    @Column(name = "recipe", nullable = false, unique = true)
+    private String recipe;
 }
